51faa54cf528e7fff92b12c535f9769cd5deae95
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-08-09 17:57:17 -0300

    Lista de laços de repetição, exercício 1

---
+++ Exerc1LacoRepeticao.java
@@ -0,0 +1,17 @@
+package AprendendoJava;
+
+public class Exerc1LacoRepeticao {
+	
+	public static void main(String[] args) {
+		
+		int num;
+		
+		for(num=1000; num<=1999;num++) {
+			if(num %11 == 5) {
+				System.out.println("Número: "+num);
+			}
+		}
+	
+	}
+
+}
