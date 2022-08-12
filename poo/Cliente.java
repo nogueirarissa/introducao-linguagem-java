ff8dbaebb3f58bb97dc96c202e456b73923aae81
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-08-12 19:37:35 -0300

    Exercício 1, cria a classe Cliente.

---
+++ poo/Cliente.java
@@ -0,0 +1,80 @@
+package AprendendoJava;
+
+public class Cliente {
+	
+	private int id;
+	private String cpf;
+	private String nome;
+	private String endereco;
+	private String email;
+	private int telefone;
+	
+	public Cliente(int id, String cpf, String nome, String endereco, String email, int telefone) { //metodo construtor
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public void visualizar() {
+		System.out.println("Código identificador do cliente: "+id);
+		System.out.println("CPF do cliente: "+cpf);
+		System.out.println("Nome do cliente: "+nome);
+		System.out.println("Enereço do cliente: "+endereco);
+		System.out.println("E-mail do cliente: "+email);
+		System.out.println("Telefone do cliente: "+telefone);
+	
+	}
+	
+}
---
+++ poo/TestaCliente.java
@@ -0,0 +1,12 @@
+package AprendendoJava;
+
+import AprendendoJava.Cliente;
+
+public class TestaCliente {
+	public static void main(String[] args) {
+		
+		Cliente c1 = new Cliente(1, "478596123-3","Luís Guerreiro","Praia","luis_bora_meu_povo@generation.com.br",912345678);
+		c1.visualizar();
+	}
+
+}
